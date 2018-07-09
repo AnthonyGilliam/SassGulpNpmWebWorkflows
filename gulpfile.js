0e44f78158db3f9c09bb375e9525d9576e72357e
Anthony <anthony.gilliam@gmail.com>
2018-07-09 17:19:55 -0400

    3:9 - Adding Browsersync, gulp-watch, and path/glob constants

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,33 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const browserSync = require('browser-sync');
+const reload = browserSync.reload;
+
+const SOURCEPATHS = {
+    sass: 'src/scss/*.scss'
+};
+const APPPATHS = {
+    root: 'app/',
+    css: 'app/css',
+    js: 'app/js'
+};
 
 gulp.task('sass', () => {
-   return gulp.src('src/scss/app.scss')
+   return gulp.src(SOURCEPATHS.sass)
        .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
-       .pipe(gulp.dest('app/css'));
+       .pipe(gulp.dest(APPPATHS.css));
 });
 
-gulp.task('default', gulp.series('sass'));
+gulp.task('serve', gulp.series('sass', () => {
+    browserSync.init([APPPATHS.root + '/*.html', APPPATHS.css + '/*.css', APPPATHS.js + '/*.js'], {
+        server: {
+            baseDir: APPPATHS.root
+        }
+    })
+}));
+
+gulp.task('watch', () => {
+    gulp.watch([SOURCEPATHS.sass], gulp.series('sass'))
+});
+
+gulp.task('default', gulp.series('sass', 'watch'));
