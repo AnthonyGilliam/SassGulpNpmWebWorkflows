8d62efd204f032c6eb3e918d9bc098a4626f151b
Anthony <anthony.gilliam@gmail.com>
2018-07-10 15:31:28 -0400

    3:12 - Cleaning folders and falls using gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,43 @@
 const gulp = require('gulp');
+const del = require('del');
+const clean = require('gulp-clean');
 const autoprefixer = require('gulp-autoprefixer');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync');
 const reload = browserSync.reload;
 
 const SOURCEPATHS = {
-    sass: 'src/scss/*.scss',
-    html: 'src/**/*.html'
+    html: 'src/**/*.html',
+    sass: 'src/scss/*.scss'
 };
 const APPPATHS = {
     root: 'app/',
     css: 'app/css',
     js: 'app/js'
 };
+
+/*Deprecated*/
+// gulp.task('clean-html', () => {
+//     return gulp.src(APPPATHS.root = '/*.html', { read: false, force: true })
+//         .pipe(clean())
+// });
+//
+// gulp.task('clean-css', () => {
+//     return gulp.src(APPPATHS.css = '/*.css', { read: false, force: true })
+//         .pipe(clean())
+// });
+
+gulp.task('clean-html', (done) => {
+    del(APPPATHS.root + '/**/*.html');
+    done();
+});
+
+gulp.task('clean-css', (done) => {
+    del(APPPATHS.css + '/**/*.css');
+    done();
+});
+
+gulp.task('clean', gulp.series('clean-html', 'clean-css'));
 
 gulp.task('copy', () => {
     return gulp.src(SOURCEPATHS.html)
@@ -26,17 +51,19 @@ gulp.task('sass', () => {
        .pipe(gulp.dest(APPPATHS.css));
 });
 
-gulp.task('serve', gulp.series('sass', () => {
+gulp.task('serve', () => {
+    gulp.watch([SOURCEPATHS.html], gulp.series('copy'));
+    gulp.watch([SOURCEPATHS.sass], gulp.series('sass'));
     browserSync.init([APPPATHS.root + '/*.html', APPPATHS.css + '/*.css', APPPATHS.js + '/*.js'], {
         server: {
             baseDir: APPPATHS.root
         }
     })
-}));
+});
 
-gulp.task('watch', gulp.series('sass', () => {
-    gulp.watch([SOURCEPATHS.sass], gulp.series('sass'));
+gulp.task('watch', () => {
     gulp.watch([SOURCEPATHS.html], gulp.series('copy'));
-}));
+    gulp.watch([SOURCEPATHS.sass], gulp.series('sass'));
+});
 
-gulp.task('default', gulp.series('copy', 'sass', 'watch'));
+gulp.task('default', gulp.series('clean', 'copy', 'sass', 'serve'));
